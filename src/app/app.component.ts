d1a4cefadc239530d23a33bf16830837a655519b
yashiro81919 <yashiro81919@hotmail.com>
2024-01-14 16:45:35 +0800

    revert to old menu and some look and feel changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,50 +1,16 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { CommonService } from './services/common.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit, OnDestroy {
+export class AppComponent implements OnInit {
 
-  menuClass?: string;
-  outletClass?: string;
-
-  subscriptions: Array<Subscription> = [];
-
-  constructor(private readonly cs: CommonService) { }
+  constructor() { }
 
   ngOnInit(): void {
     localStorage.clear();
-    this.initWidth();
-    this.listenMenuBarStatus();
   }
 
-  ngOnDestroy(): void {
-    this.subscriptions.forEach(subscription => subscription.unsubscribe());
-  }
-
-  initWidth(): void {
-    if (this.cs.isMobile()) {
-      this.menuClass = 'col-0';
-      this.outletClass = 'col-12';
-    } else {
-      this.menuClass = 'col-2';
-      this.outletClass = 'col-10';
-    }
-  }
-
-  listenMenuBarStatus(): void {
-    const sub = this.cs.menuBarStatus.subscribe({
-      next: status => {
-        if (!this.cs.isMobile()) {
-          this.menuClass = this.menuClass === 'col-2' ? 'col-0' : 'col-2';
-          this.outletClass = this.outletClass === 'col-10' ? 'col-12' : 'col-10';
-        }
-      }
-    });
-    this.subscriptions.push(sub);
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,24 +9,22 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './login/login.component';
-import { MenuComponent } from './menu/menu.component';
 import { AuthInterceptor } from './services/auth-interceptor';
 
 import { DialogModule } from 'primeng/dialog';
 import { ButtonModule } from 'primeng/button';
 import { ToastModule } from 'primeng/toast';
 import { InputTextModule } from 'primeng/inputtext';
+import { MenuModule } from 'primeng/menu';
 import { MessageService } from 'primeng/api';
 import { ToolbarModule } from 'primeng/toolbar';
-import { SidebarModule } from 'primeng/sidebar';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    LoginComponent,
-    MenuComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -38,8 +36,8 @@ import { SidebarModule } from 'primeng/sidebar';
     ButtonModule,
     ToastModule,
     InputTextModule,
-    ToolbarModule,
-    SidebarModule
+    MenuModule,
+    ToolbarModule
   ],
   providers: [{
     provide: HTTP_INTERCEPTORS,
